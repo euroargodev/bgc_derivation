801be1f67cbcc7e52466ac865e82b97f64cf0546
Matthew Cazaly <matthewcaz@gmail.com>
2021-04-26 17:15:44 +0100

    Correct A, C and D constant precision

--- bgc_derivation/nitcalc.m
+++ bgc_derivation/nitcalc.m
@@ -1,9 +1,9 @@
 function nitrate = nitcalc(pres, temp, psal, nitrate_uv, nitrate_uv_dark, nitrate_temp, e_nitrate, e_swa_nitrate, optical_wavelength_uv, nitrate_uv_ref, optical_wavelength_offset, fit, temp_cal_nitrate)
     addpath("submodules/gibbs_seawater/Toolbox");
-    a = 1.1500;
+    a = 1.1500276;
     b = 0.0284;
-    c = -0.3101;
-    d = 0.0012;
+    c = -0.3101349;
+    d = 0.001222;
 
     fit_e_nitrate = e_nitrate(fit);
     fit_wavelength = optical_wavelength_uv(fit);
